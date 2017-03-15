a7b9fe25b880110d4660568d77f85b2e008c7ea9
msimiste <mikesimister@shaw.ca>
2017-03-14 18:47:50 -0600

    proof for IPv4 comparison is working

--- Assignment4/proof.py
+++ Assignment4/proof.py
@@ -11,81 +11,33 @@ import time
 import random
 import string
 import os
-from Crypto.Cipher import AES
-from cryptoUtil import cryptoUtil
 import hashlib
 import hmac
+import ipaddress
 
 BUFFER_SIZE = 4096
 READ = False
 FILENAME = ''
 
 def main():
-  
-    key = '0123456789abcd'
-    IV =  os.urandom(16)
     
-    #dec = cryptoUtil(key,IV,'aes256')
-    
-    #plaintext = 'Mike Simister was here on friday '
-    #pLen = len(plaintext)
-    #print(pLen)
-    #while(len(plaintext) % 16 != 0):
-   #     plaintext += 'a'
-    #ctext = dec.encrypt(plaintext)
-    #print(ctext)
-    #chopped = ctext[0:16]
-    #print(chopped)
-    #output = dec.decrypt(chopped)
-    #out2 = dec.decrypt(ctext)
-    #while(len(out2) <> pLen):
-    #   out2 = out2[0:len(out2)-1]
-    
-    #test = dec.addPadding('mikeheremikeherem')
-    #test = dec.encrypt(test)
-    #print(test)
-    #print(output)
-    #print(out2)
-    #test = dec.decrypt(test)
-    #test2 = dec.removePadding(test)
-    #print(test2)
-    hastTest = "testing"
-    hashTest = hashlib.md5()
-    hashTest.update("message")
-    print(hashTest.hexdigest())
-    print(hashTest.digest_size)
-    message = 'message'
-    print(message.encode("hex"))
-    
-    message += hashTest.hexdigest();
-    temp = len(message) - 32
-    print(message)
-    print(message.encode('hex'))
-    print(message.encode('hex')[temp:])
-    hashVal = message[temp:]
-    m2 = message[:temp]
-    print(hashVal)
-    m3 = hashlib.md5()
-    m3.update(m2)
-    
-    print(m2)
-    print(hmac.compare_digest(hashVal,m3.hexdigest()))
-    #print(os.getcwd())
     fileName = sys.argv[1]
-    getFileSize(fileName)
-    #keyGenTest()
-
-def getFileSize(fileName):
-    path = os.getcwd() + "/" + fileName
-    print (path)
-    print(os.path.getsize(path))
-    print(os.system('df -k /'))
-    s = os.statvfs('/')
-    temp = (s.f_bavail * s.f_frsize)  / 1024
-    print(temp)
+    one = 1
+    zero = 0
     
+    with open(fileName) as f:
+        content = [x.strip() for x in f.readlines()]
+        rules = [x.split() for x in content]
+    ports = [0] * 65535
+    print (subNetProof('136.159.1.0','136.159.1.1/32'))
+    if(one):
+        print(one)
+    if(not zero):
+        print(zero)
+    print(rules)
+    
+def subNetProof(ip, nw):
+    return ipaddress.ip_address(ip) in ipaddress.ip_network(nw)
 
-def keyGenTest():
-    dec = cryptoUtil(key,IV)
 if __name__ == '__main__':
     main()
